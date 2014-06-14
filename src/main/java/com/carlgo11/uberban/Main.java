9acb97b6ec34beb1e6420e1bcd174afaf82417a6
Carlgo11 <carlgo11@carlgo11.com>
2014-06-14 17:48:28 +0200

    initial commit

---
+++ src/main/java/com/carlgo11/uberban/Main.java
@@ -0,0 +1,13 @@
+package com.carlgo11.uberban;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    
+    
+    public void onEnable(){
+        //getServer().getPluginManager().registerEvents(new JoinEvent(this), this);
+    }
+    
+
+}
